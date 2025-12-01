99bfb72f67f207adeba4925a17df7da52a3f858e
TerryHung <terryhung@mail.intai.com.tw>
2025-12-01 15:04:18 +0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.env.NODE_ENV === 'development';
-const base = '/challenge-qr-code-component';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -18,7 +17,7 @@ const config = {
 		}),
 
 		paths: {
-			base: dev ? '' : base
+			base: dev ? '' : '/challenge-qr-code-component'
 		},
 
 		prerender: {
