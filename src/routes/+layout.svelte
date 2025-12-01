f998821714b3735ad29c735ed23e52c1dfde88cb
TerryHung <terryhung@mail.intai.com.tw>
2025-12-01 13:19:18 +0800

    update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon-32x32.png';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="flex items-center justify-center h-screen">
+  <div class="w-[320px] h-[499px] rounded-[20px] bg-white">
+    <div class="px-[16px] pt-[16px] pb-[40px]">
+      <img src="/images/image-qr-code.png" class="rounded-[10px]" alt="qrcode">
+      <div class="mt-[24px]">
+        <p class="font-sans font-bold text-center mb-[16px] text-[22px] leading-[1.2] tracking-[0px]
+          text-slate-900" style="font-family: Outfit, sans-serif;">
+          Improve your front-end skills by building projects
+        </p>
+        <p class="font-normal text-center text-[15px] leading-[1.4] tracking-[0.2px]
+          text-slate-500" style="font-family: Outfit, sans-serif;">
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
