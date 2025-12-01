6d68a19fcc381fa85d21d4e79995396a24402e4c
TerryHung <terryhung@mail.intai.com.tw>
2025-12-01 14:00:19 +0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,29 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+const base = '/challenge-qr-code-component';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+
+		paths: {
+			base: dev ? '' : base
+		},
+
+		prerender: {
+			entries: ['/', '/404']
+		}
 	}
 };
 
